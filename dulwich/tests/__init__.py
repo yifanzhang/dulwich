b64a9fde8aa3c24a9abd9b293b96ca98b2fb8fe1
Chris Eberle <eberle1080@gmail.com>
2011-10-31 22:16:18 -0600

    Walk tests work without any modifications. That's a first.

--- dulwich/tests/__init__.py
+++ dulwich/tests/__init__.py
@@ -87,7 +87,7 @@ def self_test_suite():
         'protocol',
         'repository',
         'server',
-        #'walk',
+        'walk',
         #'web',
         ]
     module_names = ['dulwich.tests.test_' + name for name in names]
