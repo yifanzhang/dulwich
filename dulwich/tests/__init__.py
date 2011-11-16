179c34f53b8ed142aa6efac1afff23425eb3318c
Chris Eberle <eberle1080@gmail.com>
2011-11-15 20:13:06 -0700

    doctests work with Sha1Sum objects

--- dulwich/tests/__init__.py
+++ dulwich/tests/__init__.py
@@ -92,7 +92,7 @@ def tutorial_test_suite():
     tutorial = [
         'introduction',
         'repo',
-        #'object-store',
+        'object-store',
         'conclusion',
         ]
     tutorial_files = ["../../docs/tutorial/%s.txt" % name for name in tutorial]
